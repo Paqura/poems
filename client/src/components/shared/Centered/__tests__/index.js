1a0d7395ab1b95b51bc925e53ceb2c8dccaee343
paqura <slavaavals616@gmail.com>
2019-04-03 23:04:38 +0400

    Add basic snapshot test

--- client/src/components/shared/Centered/__tests__/index.js
+++ client/src/components/shared/Centered/__tests__/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components';
+
 import Centered from '../';
 
 describe('Centered', () => {
-	it('should be correct render any child', () => {
-		const instance = shallow(
+	let instance;
+
+	beforeEach(() => {
+		instance = shallow(
 			<Centered>
 				<div>Test</div>
 			</Centered>
 		);
-
+	})
+	it('should be correct render a child', () => {
 		expect(instance.find('div')).toHaveLength(1);
+	});
+
+	it('should be return snapshot', () => {
+		instance = renderer.create(
+			<Centered>
+				<div>Test 2</div>
+			</Centered>
+		).toJSON();
+
+		expect(instance).toMatchSnapshot();
 	});
 });
