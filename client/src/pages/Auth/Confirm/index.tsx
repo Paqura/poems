a011ab258e86cd64a9c2b2faa44822dd2dd02ed5
paqura <bloodking666@mail.ru>
2019-04-07 15:10:17 +0400

    Fix syntax error

--- client/src/pages/Auth/Confirm/index.tsx
+++ client/src/pages/Auth/Confirm/index.tsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 const
 	Confirm = (props: any) =>
 	<div>
-		<Link to="/sign-in">Comfirm</Link>
+		<Link to="/sign-in">Confirm</Link>
 	</div>
 
 export default Confirm;
