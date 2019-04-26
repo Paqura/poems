0a7c7bed8c8630cf90283c7ef74d5b99f465daef
paqura <bloodking666@mail.ru>
2019-04-26 12:24:48 +0400

    Add basic animation

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Transition} from 'react-transition-group';
 import {BackGround, Hero, PreTitle, Title} from './styles';
+import styled from 'styled-components';
+
+export const Fade: any = styled.div`
+  transition: 5s;
+  opacity: ${(props: any) => (props.state === 'entered' ? 1 : 0)};
+`;
 
 const
-	Home = () =>
-	<BackGround>
-		<Hero>
-			<PreTitle>Author</PreTitle>
-			<Title>SlavalS</Title>
-		</Hero>
-	</BackGround>;
+	Home = () => {
+		const [animate, setAnimate] = useState(false);
+
+		useEffect(() => {
+			setTimeout(() => setAnimate(true));
+		}, []);
+
+		return (
+			<BackGround>
+				<Transition in={animate} timeout={0} mountOnEnter>
+					{(state: any) =>
+						<Fade state={state}>
+							<Hero>
+								<PreTitle>Author</PreTitle>
+								<Title>SlavalS</Title>
+							</Hero>
+						</Fade>}
+				</Transition>
+			</BackGround>
+		);
+	};
 
 export default Home;
